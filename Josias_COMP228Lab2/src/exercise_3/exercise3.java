0d0d35fe06ab601eb2348a8afe23889eb885cffe
jndammef <jndammef@my.centennialcollege.ca>
2025-10-03 23:16:31 -0400

    fix: commit exercise 3

--- Josias_COMP228Lab2/src/exercise_3/exercise3.java
+++ Josias_COMP228Lab2/src/exercise_3/exercise3.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+package exercise_3;
+
+import java.time.Year;
 
 public class exercise3 {
+
+    //I want to implement method overloading to calculate one's age
+    // input Type:  String
+    public static int calculateAge(String birthYear) {
+        int year = Integer.parseInt(birthYear);
+        return Year.now().getValue() - year;
+    }
+
+    //input type: integer
+    public static int calculateAge(int yearOfBirth) {
+        return Year.now().getValue() - yearOfBirth;
+    }
+
+    //input type: double
+    public static double calculateAge(double yearOfBirth) {
+        double age = Year.now().getValue() - yearOfBirth;
+        return age;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Age Calculator\n");
+
+        System.out.println("Our program will select the right method base on the parameter select");
+
+        String birthYearStr = "2000";
+        int age1 = calculateAge(birthYearStr);
+        System.out.println("Using the string method, Age is " + age1 + " years\n");
+
+        int birthYearInt = 1995;
+        int age2 = calculateAge(birthYearInt);
+        System.out.println("Using the integer method, Age is " + age2 + " years\n");
+
+        double birthYearDouble = 1998.5;
+        double age3 = calculateAge(birthYearDouble);
+        System.out.printf("Using the double method, " + "Age = %.1f years\n\n", age3);
+    }
 }
